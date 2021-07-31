b55809b20ad879c9106c7524ab5fabebabb53cab
yuseogi0218 <yuseogi0218@gmail.com>
2021-07-31 12:24:03 +0900

    basic_spring

---
+++ src/main/java/spring/finEdu/controller/HelloController.java
@@ -0,0 +1,41 @@
+package spring.finEdu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import spring.finEdu.entity.Hello;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello-mvc")
+    public String helloTemplate(String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(String name) {
+        return String.format("Hello, %s", name);
+    }
+
+    @GetMapping("hello-map")
+    @ResponseBody
+    public Map<String, Object> helloMap(String name) {
+        Map<String, Object> m = new HashMap<>();
+        m.put("name", name);
+        return m;
+    }
+
+    @GetMapping("hello-object")
+    @ResponseBody
+    public Hello helloObject(String name) {
+        Hello h = new Hello(name);
+        return h;
+    }
+}
---
+++ src/main/java/spring/finEdu/entity/Hello.java
@@ -0,0 +1,11 @@
+package spring.finEdu.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Hello {
+    private String name;
+
+}
